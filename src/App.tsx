df9bd70f94eef7d81a18d495b722d4697a410255
Ana Mickaelly <ana.aguiar@seno-ti.com.br>
2023-03-08 23:10:34 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Category from './pages/Category'
+
+function App() {
+
+
+  return (
+    <div className="App">
+      <Category/>
+      <h2>oi</h2>
+    </div>
+  )
+}
+
+export default App
