516a3e9c334a745b5b2dcf8d2946e5ad5dd781c7
Ana Mickaelly <ana.aguiar@seno-ti.com.br>
2023-03-10 12:07:11 -0300

    fix: go to home

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -7,7 +7,11 @@ export const Header = () => {
   const navigate = useNavigate();
 
   const navigateToCategory = (type: string) => {
-    navigate(`category/${type}`);
+    navigate(`/category/${type}`);
+  };
+
+  const navigateToHome = () => {
+    navigate("/");
   };
 
   const menu = {
@@ -27,7 +31,9 @@ export const Header = () => {
               <img src={telephone} alt="Telefone para contato" />
               <p>11 4191 4399</p>
             </div>
-            <h2 className="title-header">Aviato</h2>
+            <h2 onClick={navigateToHome} className="title-header">
+              Aviato
+            </h2>
             <div className="infos-store-item">
               <img src={bag} alt="Sacola de compras" />
               <p>
@@ -38,7 +44,7 @@ export const Header = () => {
         </div>
       </div>
       <ul className="menu-itens">
-        <li onClick={() => navigateToCategory(menu.home)}>{menu.home}</li>
+        <li onClick={() => navigateToHome()}>{menu.home}</li>
         <li onClick={() => navigateToCategory(menu.shirt)}>{menu.shirt}</li>
         <li onClick={() => navigateToCategory(menu.blouse)}>{menu.blouse}</li>
         <li onClick={() => navigateToCategory(menu.pants)}>{menu.pants}</li>
