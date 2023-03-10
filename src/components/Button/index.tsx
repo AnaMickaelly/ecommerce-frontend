098b2c0acbbb93bf3c143bd20656356ab30640fd
Ana Mickaelly <ana.aguiar@seno-ti.com.br>
2023-03-09 21:05:24 -0300

    fix: customize button component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,9 +1,18 @@
 import { ButtonProps } from "./types";
 import "./styles.scss";
 
-export const Button = ({ children, onClick }: ButtonProps) => {
+export const Button = ({
+  children,
+  onClick,
+  uppercase,
+  width,
+}: ButtonProps) => {
   return (
-    <button onClick={onClick} className="button-container">
+    <button
+      onClick={onClick}
+      className={`button-container ${uppercase ? "uppercase" : ""}`}
+      style={{ maxWidth: `${width}px` }}
+    >
       {children}
     </button>
   );
--- src/components/Button/types.ts
+++ src/components/Button/types.ts
@@ -3,4 +3,6 @@ import React from "react";
 export type ButtonProps = {
   children: React.ReactNode;
   onClick?: () => void;
+  uppercase?: boolean;
+  width?: number;
 };
